c5f35bb5843d39c7270cbd23c2d9653da485ba61
Rominoushka <rom1.landrieau@gmail.com>
2026-06-29 21:16:54 +0200

    Bump cache for V13 amount month visibility rule

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='budget-2026-v13-amount-mode';
+const CACHE_NAME='budget-2026-v13-amount-mode-131';
 const ASSETS=['./','./index.html','./app.css','./v9.css','./v9-login.css','./v10-dark.css','./v12-product.css','./v12-full-dark.css','./v13-amount-mode.css','./app.js','./data.js','./v9.js','./v12-product.js','./v13-amount-mode.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
